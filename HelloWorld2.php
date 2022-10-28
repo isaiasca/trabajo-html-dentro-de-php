4de8df911fbc338f44b924bb17e60976a5b8f3ff
isaiasca <1307759@senati.pe>
2022-10-28 13:01:22 -0500

    segundo mejorado

--- HelloWorld2.php
+++ HelloWorld2.php
@@ -1,6 +1,13 @@
 <?php
     $nombre="holamundo";
-    print("<p>$nombre</p>")
-
-    
+    print("<h1>$nombre</h1>");
+    echo("<h1>$nombre</h1>");
+    echo '
+    <select>
+        <option>uno</option>
+        <option>dos</option>
+        <option>tres</option>
+        <option>cuatro</option>
+    </select>
+    '
 ?>
